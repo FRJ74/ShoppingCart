5a707f5308b4d7e53bd4a5b217f70f3bbe6ded8d
FRJ74 <frankr74@gmail.com>
2024-10-07 11:03:22 -0600

    Get elements from DOM

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ const cartTotal = document.getElementById("total");
 const showHideCartSpan = document.getElementById("show-hide-cart");
 
 let isCartShowing = false;
+
+const products = [
+    {
+        id: 1,
+        name: "Vanilla Cupcakes (6 Pack)",
+        price: 12.99,
+        category: "Cupcake",
+    }
+];
